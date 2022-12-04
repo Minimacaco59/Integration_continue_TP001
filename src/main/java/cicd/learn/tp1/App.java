33f526f028c9a947fba67450e8dc8eb3db82f31f
Minimacaco59 <117276158+Minimacaco59@users.noreply.github.com>
2022-12-04 11:44:03 +0100

    BUG1

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        //commentaire feature 2 
+        //commentaire feature 3
+        //commentaire B001
         System.out.println( "Hello World! par CICD rocks!" );
     }
 }
