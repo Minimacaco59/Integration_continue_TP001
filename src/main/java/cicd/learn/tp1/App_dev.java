e08445712921afa7491d986e76b59e869c4470b5
Minimacaco59 <117276158+Minimacaco59@users.noreply.github.com>
2022-12-04 14:01:06 +0100

    add app_dev

---
+++ src/main/java/cicd/learn/tp1/App_dev.java
@@ -0,0 +1,16 @@
+package cicd.learn.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App_dev 
+{
+    public static void main( String[] args )
+    {
+        //commentaire feature 2 
+        //commentaire feature 3
+        //commentaire B001
+        System.out.println( "Hello World! par CICD rocks!" );
+    }
+}
